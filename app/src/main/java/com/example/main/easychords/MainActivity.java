034ea8900b18329b4b955ffd100830fbf440d096
chamale.victor <chamale.victor@gmail.com>
2019-01-30 09:10:11 -0600

    main activity comment

--- app/src/main/java/com/example/main/easychords/MainActivity.java
+++ app/src/main/java/com/example/main/easychords/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+// This is a git Test
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
